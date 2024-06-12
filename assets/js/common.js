441f69db3863d22742ba9401929fe502887af825
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-12 10:39:01 -0600

    fetches data and logs to console

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,9 @@
+// Common data
+
 const POKEAPI = {
     URL_SEARCH_BY_TYPE: "https://pokeapi.co/api/v2/pokemon/",
     URL_SEARCH_BY_NAME: "https://pokeapi.co/api/v2/type/"
 }
+
+// utility functions
+function log(msg) { console.log(msg);}
--- assets/js/search-by-type.js
+++ assets/js/search-by-type.js
@@ -1,1 +1,25 @@
-alert(POKEAPI.URL_SEARCH_BY_TYPE);
+//DEPENDENCIES
+const typeInput = $('#search-type-btn');
+const submitSearchBtn = $('#submit-search-btn');
+
+//alert(POKEAPI.URL_SEARCH_BY_TYPE);
+
+
+
+function searchByType(event) {
+    event.preventDefault();
+    
+    log(typeInput.val());//?remove
+    fetch(POKEAPI.URL_SEARCH_BY_TYPE)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+}
+
+submitSearchBtn.on('click',searchByType);
+
+//? TEST-REMOVE
+typeInput.val(3);
