600073c006d68f77561f9cff078d7de969b0490a
Adam Mauch <maucap20@wfu.edu>
2024-06-12 12:30:14 -0400

    added button to call from TCG API, added eventlistener to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,48 @@
 $('#back-button').on('click', () => {
     window.location.assign("index.html");
 });
+
+const apiCallBtnEl = document.querySelector('#api-call-btn');
+const searchResultsEl = document.querySelector('#search-results');
+
+const callPokemonTCGAPI = function () {
+  const apiUrl = 'https://api.pokemontcg.io/v2/cards/xy1-1';
+  const apiKey = 'b66dce1b-c439-4ca5-a3b3-048672d9ddd1';
+
+  fetch(apiUrl, {
+    headers: {
+      'X-Api-Key': apiKey
+    }
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      displayCardData(data);
+    })
+    .catch(function (error) {
+      alert('Unable to connect to Pokémon TCG API');
+      console.error(error);
+    });
+};
+
+const displayCardData = function (data) {
+  const card = data.data;
+  const html = `
+    <h2>${card.name}</h2>
+    <img src="${card.images.small}" alt="${card.name}">
+    <p>Set: ${card.set.name}</p>
+    <p>Type: ${card.types.join(', ')}</p>
+    <p>Rarity: ${card.rarity}</p>
+  `;
+  searchResultsEl.innerHTML = html;
+};
+
+apiCallBtnEl.addEventListener('click', callPokemonTCGAPI);
+
+  
