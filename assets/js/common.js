af54a02dcf3dc121e1101aa895f841e96ea14cc7
Alctoria <defruits2006@gmail.com>
2024-06-15 01:59:28 +0900

    addition of the all entries into the table
    Adjusted the API fetch where all 1302 pokemon is shown instantly (from the first 20 of the last issue)

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,7 +3,7 @@
 const POKEAPI = {
     URL_SEARCH_BY_TYPE: "https://pokeapi.co/api/v2/type/",
     URL_SEARCH_BY_NAME: "https://pokeapi.co/api/v2/pokemon/",
-    URL_GET_INDEX_OF_ALL: "https://pokeapi.co/api/v2/pokemon/"
+    URL_GET_INDEX_OF_ALL: "https://pokeapi.co/api/v2/pokemon?limit=1302&offset=0"
 }
 
 const STRINGS = {
